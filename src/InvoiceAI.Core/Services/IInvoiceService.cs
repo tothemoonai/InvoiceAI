6967c9a7faa9c5f0a79715049aa40635c8078fe5
tothemoonai <tothemoonai2000@gmail.com>
2026-04-09 16:13:57 +0900

    feat: 增加按创建时间范围查询已确认发票的服务方法

--- src/InvoiceAI.Core/Services/IInvoiceService.cs
+++ src/InvoiceAI.Core/Services/IInvoiceService.cs
@@ -9,6 +9,8 @@ public interface IInvoiceService
     Task<List<Invoice>> GetByDateRangeAsync(DateTime start, DateTime end);
     Task<List<Invoice>> GetUnconfirmedAsync();
     Task<List<Invoice>> GetConfirmedAsync();
+    Task<List<Invoice>> GetByCreateDateRangeAsync(DateTime start, DateTime end);
+    List<string> GetDistinctCategories();
     Task<Invoice?> GetByIdAsync(int id);
     Task<Invoice> SaveAsync(Invoice invoice);
     Task UpdateAsync(Invoice invoice);
